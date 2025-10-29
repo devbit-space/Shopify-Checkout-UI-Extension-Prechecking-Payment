935898ac30b2eb74c63ba5e72ba22d17c07692ce
m_million_m <alexlowe00126@outlook.com>
2025-10-30 07:26:13 +0900

    feat: add the UI-extension in Thankyou page

--- extensions/checkout-ui/shopify.d.ts
+++ extensions/checkout-ui/shopify.d.ts
@@ -5,3 +5,9 @@ declare module './src/Checkout.jsx' {
   const shopify: import('@shopify/ui-extensions/purchase.checkout.actions.render-before').Api;
   const globalThis: { shopify: typeof shopify };
 }
+
+//@ts-ignore
+declare module './src/Cartline-list.jsx' {
+  const shopify: import('@shopify/ui-extensions/purchase.thank-you.cart-line-list.render-after').Api;
+  const globalThis: { shopify: typeof shopify };
+}
---
+++ extensions/checkout-ui/src/Cartline-list.jsx
@@ -0,0 +1,35 @@
+import '@shopify/ui-extensions/preact';
+import { render } from 'preact';
+import { useState } from 'preact/hooks';
+
+export default async function ExtensionRender() {
+    render(<Extension />, document.body);
+}
+
+function Extension() {
+    let variant = shopify.lines.value;
+    const [isShowDetail, setShowDetail] = useState(false);
+
+    console.log('===>', isShowDetail)
+
+    return (
+        <s-stack gap='base'>
+            <s-button onClick={() => setShowDetail(!isShowDetail)}>
+                {!isShowDetail ? 'Open Detail' : 'Close Detail'}
+            </s-button>
+            {
+                isShowDetail ? (
+                    variant.length > 0 ? (
+                        variant.map((val, index) => <s-stack key={index}>
+                            <s-image src={val.merchandise.image.url} alt={val.merchandise.image.altText} inlineSize="auto" borderRadius='small' loading='lazy' />
+                            <s-text>{`${val.merchandise.product.productType}: ${val.quantity}`}</s-text>
+                        </s-stack>)
+                    ) : (
+                    <s-text>Add a free gift to my order</s-text>
+                    )
+                )
+                : null
+            }
+        </s-stack>
+    );
+}
